5aeb5870859f8c8f2c039c876a0cdcd8514fe58b
Anushka Sahgal <sahgalanushka05@gmail.com>
2023-04-04 00:10:07 +0530

    first commit

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -35,7 +35,7 @@ const Cryptocurrencies = ({simplified}) => {
                     <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
                         <Link key={currency.id} to={`/crypto/${currency.id}`}>
                             <Card  title={`${currency.rank}. ${currency.name}`}
-                            extra={<img className='crypto-image' src={currency.iconUrl} />}
+                            extra={<img alt='img1' className='crypto-image' src={currency.iconUrl} />}
                             hoverable>
                             <p> Price: {millify(currency.price)}</p>
                             <p> Market Cap: {millify(currency.marketCap)}</p>
